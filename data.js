aa4bd6ae658ec2eb90b882d9b398a12bd02a9a8b
wonjae jun <woncha7764@gmail.com>
2022-06-26 23:41:05 +0900

    init

---
+++ data.js
@@ -0,0 +1,28 @@
+ const authors = [
+  { id: 1, name: 'J. K. Rowling'},
+  { id: 2, name: 'J. R. R. Tolkien' },
+  { id: 3, name: 'Brent Weeks'}
+]
+
+ const books = [
+  {
+    id: 1, name: 'Harry Potter and the Chamber of Secrets',
+    authorsId: 1
+  },
+  {
+    id: 2, name: 'Harry Potter and the Prisoner of Azkaban',authorsId: 1
+    
+  },
+  {
+    id: 3, name: 'Harry Potter and the Goblet of Fire',authorsId: 1
+  },
+  {
+    id: 4, name: 'The Fellowship of the Ring',authorsId: 2
+  },
+  {
+    id: 5, name: 'The Retirm of the King',authorsId: 2
+  },
+  { id: 6, name: 'The Two Towers', authorsId: 2 },
+  { id: 7, name: 'The Way of Shadows', authorsId: 3},
+  { id: 8, name: 'Beyond the Shadows', authorsId: 3 },
+]
